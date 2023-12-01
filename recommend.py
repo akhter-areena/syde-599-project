12af4dbe3dcaeea158462d1b8cb0479e6053fb2d
Maria Mihu <mvmihu@gmail.com>
2023-12-01 12:54:17 -0500

    adding more files that i forgot

---
+++ recommend.py
@@ -0,0 +1,120 @@
+import argparse
+import pandas as pd
+import numpy as np
+from pprint import pprint
+
+from models.NNeighClassifier import NNeighClassifier
+from models.SiamNet import SiamNetClassifier
+from eval.Evaluate import Evaluate
+
+class RecommendationEngine:
+    """
+    Args:
+        numFiles (int): CLI variable that determines how many MPD files to read
+        retrainNNC (bool): determines whether to retrain NNC or read from file
+
+    Attributes:
+        NNC (NNeighClassifier): NNeighbor Classifier used for predictions
+        playlists (DataFrame): contains all playlists read into memory
+        tracks (DataFrame): all tracks read into memory
+        playlistSparse (scipy.CSR matrix) playlists formatted for predictions
+    """
+    def __init__(self, models, train):
+        # Read in the relevant train, test data and features
+        self.readData()
+        self.train = train
+
+        # Initialize an empty model dictionary
+        self.models = self.buildClassifiers(models)
+    
+    def readData(self):
+        """
+        Read song and playlist data from pickled dataframes. 
+        """
+        # Read data
+        print("Reading data")
+        self.playlists_all = pd.read_pickle("lib/playlists_all.pkl")
+        self.playlists_train = pd.read_pickle("lib/playlists_train.pkl")
+        self.playlists_test = pd.read_pickle("lib/playlists_test.pkl")
+        self.tracks = pd.read_pickle("lib/tracks.pkl")
+        self.playlistSparse = pd.read_pickle("lib/playlistSparse.pkl")
+        self.playlistSparseForCNN = np.load("lib/new_sparse_matrix.npy")
+        print(f"Working with {len(self.playlists_train)} playlists " + 
+            f"and {len(self.tracks)} tracks")
+    
+
+    def buildClassifiers(self, model_names):
+        """
+        Init classifiers and set initial classifier as main
+        """
+        # TODO: would eventually read in all model names and build dict of models
+        return {
+            # 'nnc': self.buildNNC(
+            #     train=self.train
+            # ),
+            'cnn': self.buildSiamNet(
+                train=self.train
+            )
+        }
+
+    def buildNNC(self, train): 
+        """
+        Init NNC classifier
+        """
+        return NNeighClassifier(
+            sparsePlaylists=self.playlistSparse,
+            tracks=self.tracks,
+            playlists=self.playlists_all,
+            reTrain=train
+        ) 
+    
+    def buildSiamNet(self, train): 
+        """
+        Init NNC classifier
+        """
+        return SiamNetClassifier(
+            playlists=self.playlists_all,
+            sparsePlaylists=self.playlistSparseForCNN,
+            tracks=self.tracks,
+            reTrain=train
+        ) 
+    
+def main():
+    # Prepare command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--models', help='Enter models you want delimited by ,', type=str)
+    parser.add_argument('-t', '--train', action='store_true')
+    parser.add_argument('-f', '--first_test', action='store_true')
+    args = parser.parse_args()
+
+    model_names = [str(model) for model in args.models.split(',')]
+    print(f"Models we're comparing: {model_names}")
+
+    # Initialize recommendation engine.
+
+    # If train is true or the model doesn't exist, we retrain the model, and it gets pickled and saved to the /lib folder.
+    # If train is false, we load in the saved model, and we continue to evaluation.
+    print(f"Training is set to: {args.train}")
+    recommender = RecommendationEngine(
+        models=model_names, 
+        train=args.train)
+
+    # Evaluate the models 
+    #TODO: same as above, make the model choose between passed-in options
+    # evaluator = Evaluate(
+    #     tracks=recommender.tracks, 
+    #     model=recommender.models['nnc']
+    # )
+
+    evaluator = Evaluate(
+        tracks=recommender.tracks,
+        model=recommender.models['cnn']
+    )
+    
+    if args.first_test:
+        evaluator.obscure_and_save()
+
+    pprint(evaluator.evaluate())
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/getartists.py
@@ -0,0 +1,156 @@
+import logging
+from tqdm import tqdm
+from gensim.models import Word2Vec
+import pandas as pd
+import pandas as pd
+import numpy as np
+from sklearn.manifold import TSNE
+
+
+PATH_TO_PLAYLIST_DATA = r"lib"
+NUM_COMPONENTS = 7
+
+print("GET DATA....")
+
+tracks = pd.read_pickle(f"{PATH_TO_PLAYLIST_DATA}/tracks.pkl")
+playlists_test = pd.read_pickle(f"{PATH_TO_PLAYLIST_DATA}/playlists_test.pkl")
+playlists_train = pd.read_pickle(
+    f"{PATH_TO_PLAYLIST_DATA}/playlists_train.pkl")
+
+print("SEE IF PLAYLIST IS TOO SMALL....")
+
+# See if playlists too small
+for i in tqdm(list(playlists_train["tracks"].values)):
+    if len(i) < 2:
+        print(f'length of playlist train small! Delete! Length is {len(i)}')
+
+for i in tqdm(list(playlists_train["tracks"].values)):
+    if len(i) < 2:
+        print(f'length of playlist test small! Delete! Length is {len(i)}')
+
+
+print("REWORK DATA....")
+
+artists = tracks[['artist_name', 'sparse_id']]
+
+# Get unique id corresponding to each artist
+artists_to_id = pd.DataFrame(
+    artists['artist_name'].unique(), columns=["artist_name"])
+artists_to_id.head()
+
+artists_to_id['artist_index'] = artists_to_id.index
+
+print("GET ARTIST IDS AND SPARSE IDS FOR EACH TRAIN PLAYLIST....")
+
+# Populate another matirx consisting of sparse ids
+all_sparse_ids = []
+
+# Populate matrix consisting of playlists where each playlist consists of artist ids
+train_playlist_artists = []
+train_playlist_sparse_ids = []
+
+for i in tqdm(playlists_train['tracks'].values[:25]):
+    artist_names = artists[artists.index.isin(i)]['artist_name']
+    artist_ids = list(pd.merge(artist_names, artists_to_id,
+                      how="left")['artist_index'])
+    train_playlist_artists.append(artist_ids)
+    train_playlist_sparse_ids.append(
+        list(artists[artists.index.isin(i)]['sparse_id']))
+
+    unique_ids = artists[artists.index.isin(i)]['sparse_id'].unique()
+
+    for id in unique_ids:
+        all_sparse_ids.append(id)
+
+
+print("GET ARTIST IDS AND SPARSE IDS FOR EACH TEST PLAYLIST....")
+
+# Populate matrix consisting of playlists where each playlist consists of song ids
+test_playlist_artists = []
+test_playlist_sparse_ids = []
+
+for i in tqdm(playlists_test['tracks'].values[:5]):
+    artist_names = artists[artists.index.isin(i)]['artist_name']
+    artist_ids = list(pd.merge(artist_names, artists_to_id,
+                      how="left")['artist_index'])
+    test_playlist_artists.append(artist_ids)
+    test_playlist_sparse_ids.append(
+        list(artists[artists.index.isin(i)]['sparse_id']))
+
+    unique_ids = artists[artists.index.isin(i)]['sparse_id'].unique()
+
+    for id in unique_ids:
+        all_sparse_ids.append(id)
+
+# Get all sparse ids
+np_sparse = np.array(all_sparse_ids)
+
+# All of the sparse ids
+unique_np_sparse = np.unique(np_sparse)
+
+# Create mapping of index of sparse id to actual sparse id. This index is our matrix index.
+mappings = {id: index for index, id in enumerate(unique_np_sparse)}
+
+# import model
+model = Word2Vec.load("lib/artist_name2vec.model")
+
+print("POPULATE FINAL TRAIN PLAYLIST MATRIX....")
+
+# 256 is number of embeddings
+train_playlistArtist = np.full((len(train_playlist_artists), len(
+    unique_np_sparse), NUM_COMPONENTS), -5)
+
+# Iterate through playlists
+for i in tqdm(range(len(train_playlist_artists))):
+
+    playlistIDX = i
+    # Index of songs in playlists
+    curr_playlist_sparse_ids = train_playlist_sparse_ids[i]
+    curr_playlist_song_ids = [mappings[id] for id in curr_playlist_sparse_ids]
+
+    # Artist ids of songs in playlist
+    curr_playlist_artist_ids = train_playlist_artists[i]
+
+    # Get embeddings
+    embeddings = model.wv[curr_playlist_artist_ids]
+
+    tsne = TSNE(n_components=NUM_COMPONENTS, perplexity=5, method="exact")
+    small_embeddings = tsne.fit_transform(embeddings)
+
+    # Set index to embedding if playlist has song
+    train_playlistArtist[playlistIDX,
+                         curr_playlist_song_ids] = small_embeddings
+
+
+# Test matrix
+
+print("POPULATE FINAL TEST PLAYLIST MATRIX....")
+
+test_playlistArtist = np.full((len(test_playlist_artists), len(
+    unique_np_sparse), NUM_COMPONENTS), -5)
+
+for i in tqdm(range(len(test_playlist_artists))):
+    playlistIDX = i
+    # Index of songs in playlists
+    curr_playlist_sparse_ids = test_playlist_sparse_ids[i]
+    curr_playlist_song_ids = [mappings[id] for id in curr_playlist_sparse_ids]
+
+    # Artist ids of songs in playlist
+    curr_playlist_artist_ids = test_playlist_artists[i]
+
+    # Get embeddings
+    embeddings = model.wv[curr_playlist_artist_ids]
+    tsne = TSNE(n_components=NUM_COMPONENTS, perplexity=5, method="exact")
+    small_embeddings = tsne.fit_transform(embeddings)
+
+    # Set index to embedding if playlist has song
+    train_playlistArtist[playlistIDX,
+                         curr_playlist_song_ids] = small_embeddings
+
+
+# # Pickle file the outputs
+
+print("SAVE FINAL ARTIST AND PLAYLIST MATRICES TO FILE......")
+
+np.save("lib/artist_names_train", train_playlistArtist, allow_pickle=True)
+np.save("lib/artist_names_test", test_playlistArtist, allow_pickle=True)
